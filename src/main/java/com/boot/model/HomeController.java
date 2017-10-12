74d505cadde557937fd33f823468f8e19744faac
Yugndhar Reddy <yreddy_t@yahoo.com>
2017-10-11 22:00:55 -0500

    iniatial das-boot first commit

---
+++ src/main/java/com/boot/model/HomeController.java
@@ -0,0 +1,15 @@
+package com.boot.model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Das boot, reporting for duty!";
+	}
+
+}
